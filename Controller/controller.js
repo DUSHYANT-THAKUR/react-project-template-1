c011844dbb4c62554d42f2214264850cf5501173
Dushyant <95307878+DUSHYANT-THAKUR@users.noreply.github.com>
2024-06-13 22:15:35 +0530

    change

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,9 +1,9 @@
 let tbl_emp = require("../Models/model")
 module.exports.register = async (req, res) => {
     try {
-        let {id,email,password} = req.body;
+        let {name,email,password} = req.body;
         let details = {
-            id,
+            name,
             email,
             password
         }
@@ -19,6 +19,35 @@ module.exports.register = async (req, res) => {
         return res.status(500).json(
             {
                 message: "Internal Server Error",
+                err: err
+            }
+        )
+    }
+}
+module.exports.login = async (req, res) => {
+    try {
+        let {email,password} = req.body;
+        let result = await tbl_emp.find({email:email,password:password})
+        console.log(result)
+        if(result.length>0) {
+            return res.status(200).json(
+                {
+                    message: "login Successfully"
+                }
+            )
+        } else {
+            return res.status(404).json(
+                {
+                    message : "Wrong Credential"
+                }
+            )
+        }
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(
+            {
+                message: "Internal Server error",
                 err: err
             }
         )
--- router/route.js
+++ router/route.js
@@ -2,5 +2,6 @@ let route = require("express").Router();
 let apiController = require('../Controller/controller')
 
 route.post("/register",apiController.register)
+route.post("/login",apiController.login)
 
 module.exports = route;
