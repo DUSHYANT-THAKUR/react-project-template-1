e160e41a01ee4fbc0152b67f4d425031bde39fec
Dushyant <95307878+DUSHYANT-THAKUR@users.noreply.github.com>
2024-06-14 11:35:54 +0530

    change

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-let express = require('express')
-let app = express();
-let cors = require('cors')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const routes = require('./router/route');
+const port = 8080;
 
-let routes = require("./router/route")
-let port = process.env.PORT || 8080;
+const allowedOrigins = [
+  'https://thunderous-strudel-cc8e0e.netlify.app' // Your live front-end domain
+];
 
-app.use(cors({origin:["http://localhost:3000","http://localhost:3001"]}))
-app.use(express.urlencoded());
-app.use(express.json())
-app.use(routes)
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin like mobile apps or curl requests
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-app.listen(port,()=>{
-    console.log('running on port '+port)
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+  console.log('running on port ' + port);
+});
