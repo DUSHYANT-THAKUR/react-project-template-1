52f5c3ecce840124c82dfe10bf54ada360b85c26
Dushyant <95307878+DUSHYANT-THAKUR@users.noreply.github.com>
2024-06-21 17:35:48 +0530

    change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 8080;
 
 const allowedOrigins = [
-  'https://chimerical-heliotrope-83572d.netlify.app'
+  'elegant-kitsune-85a9bf.netlify.app'
 ];
 
 app.use((req, res, next) => {
